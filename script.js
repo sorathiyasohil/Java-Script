2239696398339cf8a6f1db47405beacefb1b9bd2
Bread <123@gmail.com>
2019-05-23 10:40:47 +0530

    array

---
+++ script.js
@@ -0,0 +1,179 @@
+/*var name ='jhon';
+console.log(name);
+
+var lastname ='mark';
+console.log(lastname);
+
+var age ='26';
+console.log(age);
+
+var birth=true
+console.log(birth);*/
+
+/*-----------------------------*/
+
+/*var name ='mark';
+var age = 26;
+console.log(name + age);
+
+console.log(age + age);
+
+var job , isMarid;
+
+job='techer';
+isMarid=false;
+
+console.log(name + ' is a ' + age + ' Year old ' + job + ' Is her Marid ? ' + isMarid + '.');
+
+job='driver';
+age='therty six'
+
+console.log(name + ' is a ' + age + ' Year old ' + job + ' Is her Marid ? ' + isMarid + '.');
+*/
+
+
+
+//Operator//////
+
+/* var birth = 2019-23;
+console.log(birth); */
+
+//If-Else Statement/////////
+
+/* var name= 'jhon';
+var age=26;
+var isMarid='yes';
+
+if(isMarid ==='yes'){
+    console.log(name  + ' is Marid')
+}else{
+    console.log(name+'Marid very Soon...')
+}
+
+isMarid=true
+
+if(isMarid){
+    console.log('YES!')
+}
+
+if(23==23){
+    console.log('Somthings Prints...');
+}
+ */
+
+
+ //boolen logic and switch//////
+
+ /* var age=35;
+
+ if(age===20){
+     console.log('jhon is young')
+ }else if (age > 20 && age < 30) {
+     console.log('jhon is middle')
+ } else {
+     console.log('jhon is man');
+ }
+
+
+ var job ='teacher';
+
+ job=prompt('enter somthing to chhose')
+
+ switch (job)
+ {
+    case 'teacher' :
+        console.log('jhon techaing');
+    break;
+    case 'dance' :
+        console.log('jhon teching Dance');
+        break;
+    case 'driver':
+        console.log('jhon driving')
+        break;
+    default:
+    console.log('somthing else..');
+    
+ } */
+
+
+//CODECHALLANGE///////////////////
+
+/* var heightjhon=170;
+var heightMike=180;
+
+var agejhon=25;
+var ageMike=20;
+
+var scorejhon = heightjhon + 5 *agejhon;
+var scoreMike = heightMike + 5 *ageMike;
+
+// if(scorejhon > scoreMike){
+//     console.log('jhonwin the game');
+// }else if(scoreMike > scorejhon){
+//     console.log('mike won the game');
+// }else if(scorejhon === scoreMike){
+//     console.log('match is draw');
+// }
+
+
+var heightMarry=195;
+var ageMarry=23;
+
+var scoreMarry = heightMarry + 5 * ageMarry;
+
+if(scorejhon > scoreMike && scorejhon > scoreMarry){
+    console.log('jhon win game');
+}else if(scoreMike > scorejhon && scoreMike >scoreMarry){
+    console.log('mike win game');
+}else if(scoreMarry > scorejhon && scoreMarry >scoreMike){
+    console.log('marry win game');
+} */
+
+
+//FUNCTION////////////////
+
+
+//STATEMENT AND EXPRESSION/////////////
+
+//ARRAY////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
